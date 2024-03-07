e2ec5e2cd40178d918f49612eb496f7da8f5590f
nicolas <bigmacnls@gmail.com>
2024-03-07 15:39:44 -0300

    atualizando a ficha

--- script/script.js
+++ script/script.js
@@ -5,14 +5,15 @@ hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 
 const habilidades = {
     alquimista: [
-        'Filósofo Natural: Você aprendeu muitas coisas sobre alquimia durante seus estudos. A partir do 1º nível, você pode adicionar metade do seu bônus de proficiência, arredondado para cima, a qualquer teste de habilidade que você fizer para identificar ervas, poções, venenos ou outras substâncias alquímicas. Se o teste de habilidade já incluir seu bônus de proficiência, você poderá adicionar esse bônus além do seu bônus de proficiência normal.',
-        'Bombas: No 1º nível, você pode criar bombas alquímicas voláteis usando os suprimentos do seu alquimista. Sempre que terminar um descanso curto ou longo, você poderá criar um número de bombas igual a 10 + o dobro do seu nível de alquimista. Para você, o custo dos materiais necessários para criar essas bombas é insignificante. Após 24 horas, uma bomba não utilizada torna-se inerte. As estatísticas de uma bomba são mostradas abaixo.'
+        'Filósofo Natural <br><br> Você aprendeu muitas coisas sobre alquimia durante seus estudos. A partir do 1º nível, você pode adicionar metade do seu bônus de proficiência, arredondado para cima, a qualquer teste de habilidade que você fizer para identificar ervas, poções, venenos ou outras substâncias alquímicas. Se o teste de habilidade já incluir seu bônus de proficiência, você poderá adicionar esse bônus além do seu bônus de proficiência normal.',
+        'Bombas <br><br> No 1º nível, você pode criar bombas alquímicas voláteis usando os suprimentos do seu alquimista. Sempre que terminar um descanso curto ou longo, você poderá criar um número de bombas igual a 10 + o dobro do seu nível de alquimista. Para você, o custo dos materiais necessários para criar essas bombas é insignificante. Após 24 horas, uma bomba não utilizada torna-se inerte. As estatísticas de uma bomba são mostradas abaixo.'
     ],
     artesao: [
         'Truque: Você conhece três truques de sua escolha da lista de magias do mago. Você aprende truques adicionais de magia do mago à sua escolha em níveis mais altos.'
     ],
     barbaro: [
-        'Ataque Furtivo: Uma vez por turno, você pode causar dano extra a um alvo que você acertou com um ataque com arma, se você tiver vantagem na jogada de ataque.'
+        'Raiva. <br> <br> Na batalha, você luta com ferocidade primitiva, No seu turno, você pode entrar em fúria como uma ação bônus, Você tem vantagem em testes de Força e testes de resistência de Força.<br><br>Quando você faz um ataque com arma corpo a corpo usando Força, você ganha um bônus na jogada de dano que aumenta à medida que você ganha níveis como um bárbaro, como mostrado na coluna Dano de Fúria da tabela Bárbaro.<br><br>Você tem resistência a danos de concussão, perfurantes e cortantes. <br>Se você for capaz de lançar feitiços, não poderá lançá-los ou se concentrar neles enquanto estiver em fúria, Sua raiva dura 1 minuto. Termina mais cedo se você ficar inconsciente ou se seu turno terminar e você não tiver atacado uma criatura hostil desde seu último turno ou sofrido danos desde então. Você também pode acabar com sua fúria no seu turno com uma ação bônus.<br><br>Depois de ter enfurecido o número de vezes mostrado para o seu nível de bárbaro na coluna Fúria da tabela Bárbaro, você deve terminar um descanso longo antes de poder entrar em fúria novamente.',
+        'Defesa. <br> <br> Não Blindada Enquanto você não estiver usando nenhuma armadura, sua classe de armadura é igual a 10 + seu modificador de Destreza + seu modificador de Constituição. Você pode usar um escudo e ainda ganhar esse benefício.'
     ]
 };
 
@@ -21,6 +22,7 @@ const habilidadeContainer = document.querySelector('.habilidadeContainer');
 const classInfo = document.getElementById('classInfo');
 const vidaSpan = document.getElementById('vida');
 const proficienciasSpan = document.getElementById('proficiencias');
+const equipamentoTexto = document.getElementById('equipamentoTexto');
 
 classSelect.addEventListener('change', function () {
     const selectedClass = classSelect.value;
@@ -31,19 +33,22 @@ classSelect.addEventListener('change', function () {
     habilidadeContainer.innerHTML = ''; // Limpa as habilidades anteriores
     habilidades[selectedClass].forEach((habilidade, index) => {
         const habilidadeElement = document.createElement('p');
-        habilidadeElement.textContent = `Habilidade ${index + 1}: ${habilidade}`;
+        habilidadeElement.innerHTML = `Habilidade ${index + 1}: ${habilidade}`;
         habilidadeContainer.appendChild(habilidadeElement);
     });
         // Atualiza o dado de vida e as proficiências
         if (selectedClass === 'alquimista') {
-            vidaSpan.textContent = '1d6';
-            proficienciasSpan.textContent = 'Armadura leve e Armas simples e bombas';
+            vidaSpan.innerHTML = '1d6';
+            proficienciasSpan.innerHTML = 'Armadura: leve <br>Arma: simples e bombas';
+            equipamentoTexto.innerHTML = 'Você começa com uma poção de cura e um conjunto de ferramentas de alquimia e bombas';
         } else if (selectedClass === 'artesao') {
-            vidaSpan.textContent = '1d6';
-            proficienciasSpan.textContent = 'Livros de Magia';
+            vidaSpan.innerHTML = '1d6';
+            proficienciasSpan.innerHTML = 'Livros de Magia';
+            equipamentoTexto.innerHTML = 'Você começa com uma poção de cura e um conjunto de ferramentas de alquimia e bombas';
         } else if (selectedClass === 'barbaro') {
-            vidaSpan.textContent = '1d8';
-            proficienciasSpan.textContent = 'Furtividade e Truques';
+            vidaSpan.innerHTML = '1d12 <br> Testes de Resistência: Força, Constituição';
+            proficienciasSpan.innerHTML = ' Armadura leve, média e escudos <br> Armas: simples e marciais';
+            equipamentoTexto.innerHTML = '(a) um machado grande ou (b) qualquer arma marcial corpo a corpo <br>(a) dois machados de mão ou (b) qualquer arma simples <br>Uma mochila de explorador e quatro dardos';
         }
 });
 
